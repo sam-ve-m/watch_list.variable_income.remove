b962a33741e67845b9e1d6b488bc513132484482
João Bonfim <jpa@lionx.com.br>
2022-06-10 12:35:34 -0300

    fix: made the symbols capital

--- func/src/domain/request/model.py
+++ func/src/domain/request/model.py
@@ -2,7 +2,7 @@
 from src.domain.enums.region.enum import Region
 
 # Third party
-from pydantic import BaseModel, constr
+from pydantic import BaseModel, constr, validator
 
 from typing import List
 
@@ -10,6 +10,11 @@ from typing import List
 class WatchListSymbol(BaseModel):
     symbol: constr(min_length=1)
     region: Region
+
+    @validator("symbol")
+    def validate_symbols(cls, symbol):
+        symbol = symbol.upper()
+        return symbol
 
 
 class WatchListSymbols(BaseModel):
