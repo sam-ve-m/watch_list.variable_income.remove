70c08ca0eb0cccbed7cc7718b14f8e79336e7548
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-26 11:08:16 -0300

    "build: changed fission strategy to poolmg"

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 fission spec init
-fission env create --spec --name wtc-list-remove-env --image nexus.sigame.com.br/fission-wacth-list-remove:0.2.0-0 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
-fission fn create --spec --name wtc-list-remove-fn --env wtc-list-remove-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
-fission route create --spec --name wtc-list-remove-rt --method DELETE --url /watch_list/variable_income/remove --function wtc-list-remove-fn
+fission env create --spec --name wtc-list-var-remove-env --image nexus.sigame.com.br/fission-wacth-list-variable-income-remove:0.2.0-0 --poolsize 2 --graceperiod 3 --version 3 --imagepullsecret "nexus-v3" --spec
+fission fn create --spec --name wtc-list-var-remove-fn --env wtc-list-var-remove-env --code fission.py --executortype poolmgr --requestsperpod 10000 --spec
+fission route create --spec --name wtc-list-var-remove-rt --method DELETE --url /watch_list/variable_income/remove --function wtc-list-var-remove-fn
